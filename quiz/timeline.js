90c1c28140727d401291062f06e583ccf7f17908
wec2t20a20hp3nea024q-4q3-q343-5hq35y-q3 <lichingshun@gmail.com>
2026-03-21 21:49:31 +0800

    Update timeline.js
    for ut2

--- quiz/timeline.js
+++ quiz/timeline.js
@@ -1,7 +1,16 @@
 // timeline.js
 const timelineEvents = [
-    { name: "DSE Chinese Exam", date: "2025-04-10T09:00:00" },
-    { name: "DSE English Exam", date: "2025-04-12T09:00:00" },
-    { name: "DSE Mathematics Exam", date: "2025-04-15T09:00:00" },
-    { name: "Summer Break", date: "2025-07-15T00:00:00" }
+    { name: "F4 2nd UT English",   date: "2026-03-23T08:30:00" },
+    { name: "F4 2nd UT Physics",   date: "2026-03-23T10:00:00" },
+    { name: "F4 2nd UT ICT",       date: "2026-03-23T11:15:00" },
+    { name: "F4 2nd UT Chinese",   date: "2026-03-24T08:30:00" },
+    { name: "F4 2nd UT M2",        date: "2026-03-24T11:15:00" },
+    { name: "F4 2nd UT Math Core", date: "2026-03-25T08:30:00" },
+    { name: "F4 2nd UT Biology",   date: "2026-03-25T10:00:00" },
+    { name: "F4 2nd UT CSD",       date: "2026-03-26T08:30:00" },
+    { name: "F4 2nd UT Chemistry", date: "2026-03-27T08:30:00" },
+    { name: "F4 Final", date: "2025-07-15T00:00:00" },
+    { name: "F4 Final", date: "2025-07-15T00:00:00" },
+    { name: "F4 Final", date: "2025-07-15T00:00:00" },
+    { name: "F4 Final", date: "2025-07-15T00:00:00" }
 ];
