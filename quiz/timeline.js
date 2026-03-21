419d9f90f5a594d2ea98962e4282da4b20c3364c
wec2t20a20hp3nea024q-4q3-q343-5hq35y-q3 <lichingshun@gmail.com>
2026-03-21 21:40:35 +0800

    Create timeline.js

---
+++ quiz/timeline.js
@@ -0,0 +1,7 @@
+// timeline.js
+const timelineEvents = [
+    { name: "DSE Chinese Exam", date: "2025-04-10T09:00:00" },
+    { name: "DSE English Exam", date: "2025-04-12T09:00:00" },
+    { name: "DSE Mathematics Exam", date: "2025-04-15T09:00:00" },
+    { name: "Summer Break", date: "2025-07-15T00:00:00" }
+];
